b02ab66f10f4d0614f9762b4abf2879bdf99141e
Omar Hammad <omar@aqwas.sa>
2022-04-29 07:28:40 -0600

    Initial Commit

---
+++ Spinners/SpinnersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpinnersApp.swift
+//  Spinners
+//
+//  Created by Omar on 29/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct SpinnersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
